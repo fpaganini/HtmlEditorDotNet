78dc741b2f8f45e2637f2e4b54a4f518bd0f9e53
fpaganini <fpaganinirodrigues@gmail.com>
2015-11-13 22:07:46 -0200

    Initial Code

---
+++ source/ASPX/4.0/InHTML/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="InHTML.Default" %>
+
+<%@ Register Src="~/htmleditor.ascx" TagPrefix="uc1" TagName="htmleditor" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>HTMLEditor for DOTNET</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" />
+    <link href="resources/pgn_html_editor.css" rel="stylesheet" />
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <uc1:htmleditor runat="server" id="htmleditor" />
+    </form>
+
+    
+
+    
+
+
+</body>
+</html>
---
+++ source/ASPX/4.0/InHTML/htmleditor.ascx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InHTML
+{
+    public partial class htmleditor : System.Web.UI.UserControl
+    {
+        public bool ToolBar_Bold { get; set; } = true;
+        public bool ToolBer_Italic { get; set; } = true;
+        public bool ToolBar_Underscore { get; set; } = true;
+        public bool ToolBar_Stryke { get; set; } = true;
+        public bool ToolBar_SubScript { get; set; } = true;
+        public bool ToolBar_SuperScript { get; set; } = true;
+        public bool ToolBar_DecreaseIndent { get; set; } = true;
+        public bool ToolBar_IncreaseIndent { get; set; } = true;
+        public bool ToolBar_InsertHorizontalLine { get; set; } = true;
+        public bool ToolBar_Undo { get; set; } = true;
+        public bool ToolBar_Redo { get; set; } = true;
+        public bool ToolBar_Clear { get; set; } = true;
+        public bool ToolBar_Select { get; set; } = true;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
